eff346a0b952689887ea279765ed298ed5f3edc8
Bogdan <lmaesrtol@gmail.com>
2023-07-27 16:31:27 +0300

    reset style

--- src/Container/App/App.tsx
+++ src/Container/App/App.tsx
@@ -1,10 +1,12 @@
 import Header from '../Header/Header'
+import CssBaseline from '@mui/material/CssBaseline'
 
 type Props = {}
 
 const App = (props: Props) => {
     return (
         <>
+            <CssBaseline />
             <Header />
         </>
     )

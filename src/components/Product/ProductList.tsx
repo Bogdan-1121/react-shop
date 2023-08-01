d52971cfd8788b2d73416fa32f5ce8d17e7dd97b
Bogdan <lmaesrtol@gmail.com>
2023-08-01 12:10:48 +0300

    added price-list

--- src/components/Product/ProductList.tsx
+++ src/components/Product/ProductList.tsx
@@ -1,4 +1,5 @@
-import { Typography } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
+import ProductListItem from './ProductListItem'
 
 type Props = {}
 
@@ -16,6 +17,35 @@ const ProductList = (props: Props) => {
             >
                 Product List
             </Typography>
+            <Grid container spacing={4}>
+                <Grid item xs={12} sm={6} md={4}>
+                    <ProductListItem
+                        title="iPhone 12"
+                        description="This is iPhone 12 Pro"
+                        type="phone"
+                        capacity="64"
+                        price={500}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <ProductListItem
+                        title="iPhone 13"
+                        description="This is iPhone 13 Pro"
+                        type="phone"
+                        capacity="128"
+                        price={1000}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <ProductListItem
+                        title="iPhone 14"
+                        description="This is iPhone 14 Pro"
+                        type="phone"
+                        capacity="256"
+                        price={1500}
+                    />
+                </Grid>
+            </Grid>
         </div>
     )
 }
---
+++ src/components/Product/ProductListItem.tsx
@@ -0,0 +1,35 @@
+import { Button, Card, CardContent } from '@mui/material'
+import './ProductListItem.scss'
+
+type Props = {
+    title: string
+    description: string
+    type: string
+    capacity: string
+    price: number
+}
+
+const ProductListItem = ({
+    title,
+    description,
+    type,
+    capacity,
+    price,
+}: Props) => {
+    return (
+        <Card variant="outlined">
+            <CardContent>
+                <h2 className="product-title">{title}</h2>
+                <p className="product-description">{description}</p>
+                <div className="product-features">Type: {type}</div>
+                <div className="product-features">Capacity: {capacity} Gb</div>
+                <div className="product-price">
+                    Price: <span>{price}</span>
+                </div>
+                <Button variant="outlined">Add to cart</Button>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default ProductListItem
